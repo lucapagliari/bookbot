a7c68525536666c7f8f95b60d08430f235c95ea8
lucapagliari <l.pagliari365@outlook.it>
2025-02-23 14:48:37 +0000

    add count_characters v1

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from stats import count_words
+from stats import count_words, count_characters
 
 def main():
     frankestein_path = "books/frankenstein.txt"
     text = get_book_text(frankestein_path)
     print(f"{count_words(text)} words found in the document")
+    print(count_characters(text))
 
 def get_book_text(filepath):
     with open(filepath) as f:
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_characters(text):
+    occurences = {}
+    for char in text:
+        c = char.lower()
+        if c in occurences:
+            occurences[c] += 1
+        else:
+            occurences[c] = 1
+    return occurences
